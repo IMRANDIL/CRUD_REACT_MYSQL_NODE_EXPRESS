e78918071b5490cab516832736108e415e7f3e7c
IMRANDIL <aliimranadil2@gmail.com>
2022-01-13 12:04:04 +0530

    yh

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ function App() {
   const [country, setCountry] = useState('');
   const [position, setPosition] = useState('');
   const [wage, setWage] = useState('');
+  const [employeeList, setEmployeeList] = useState([]);
+  const [list, setList] = useState('')
+
+
 
   const createEmployee = async () => {
     try {
@@ -25,8 +29,20 @@ function App() {
         setPosition('');
         setWage('');
         setAge('');
+
       }
 
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
+
+  const showEmployee = async () => {
+    try {
+      const data = await axios.get('http://localhost:5000/showEmployee');
+      setEmployeeList(data.data.result);
+      setList('~~Employees List~~')
     } catch (error) {
       console.log(error);
     }
@@ -55,6 +71,31 @@ function App() {
 
       </div>
       <button className='btn' onClick={createEmployee}>Add Employee</button>
+      <hr />
+      <button className="showbtn btn" onClick={showEmployee} >Show Employee</button>
+      <h1>{list}</h1>
+      <div className="employeeList">
+
+        {employeeList.map((list, index) => {
+          const { id, name, age, country, position, wage } = list;
+
+          return (
+            <div className="item" key={index}>
+              <div className="card">
+                <div className="container">
+                  <span>Id:{id}</span>
+                  <h4>Name:<b>{name}</b></h4>
+                  <p>Age:{age}</p>
+                  <p>Country:{country}</p>
+                  <p>Role:{position}</p>
+                  <p>Salary:{wage}</p>
+                </div>
+              </div>
+
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
